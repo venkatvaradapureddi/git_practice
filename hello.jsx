32ae87b77d55a70e399fda9eec31242deae0cf94
venkat varadapureddi <vvaradapureddi@gmail.com>
2025-11-21 16:38:26 +0530

    added again hello file

---
+++ hello.jsx
@@ -0,0 +1,19 @@
+hi
+import React from 'react'
+
+const hello = () => {
+  return (
+    <div>hello</div>
+  )
+}
+
+export default hello
+
+import React from 'react'
+
+export const Bye = () => {
+  return (
+    <div>Bye</div>
+  )
+}
+
